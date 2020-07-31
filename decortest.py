d182a1a887c25fc1c161dd6d57fec268e8432b25
K.M Hussain <km.hussain.in@gmail.com>
2020-07-31 11:07:08 +0530

    Uploaded from iPhone

--- decortest.py
+++ decortest.py
@@ -6,6 +6,10 @@ class Echo:
   def __call__(self, *args):
     print(f">>Invoking {self.target.__name__}")
     return self.target(*args)
+    
+  #just a helper (not required)
+  def decorates(func):
+    return isinstance(func, Echo)
 
 @Echo
 def greet(name, formal=False):
@@ -14,5 +18,29 @@ def greet(name, formal=False):
   return f'Hi {name}!'
 
 
+class Interval:
+  
+  def __init__(self):
+    self.__min = 0
+    self.__sec = 0
+    
+  @property
+  def time(self): 
+    return 60 * self.__min + self.__sec
+  
+  @time.setter
+  def time(self, value):
+    self.__min = value // 60
+    self.__sec = value % 60
+  
+  def __str__(self):
+    return(f'{self.__min}:{self.__sec:02d}')
+    
+
 print(greet('Jack'))
 print(greet("Jack's Mom", True))
+#print(Echo.decorates(greet))
+
+i = Interval()
+i.time = 125
+print(i)

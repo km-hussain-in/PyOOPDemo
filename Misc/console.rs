a8280545ed49a5e3c268a6a3462c2638d9eb494e
K.M Hussain <km.hussain.in@gmail.com>
2020-08-17 09:55:58 +0530

    Testing

--- Misc/console.rs
+++ Misc/console.rs
@@ -1,13 +1,8 @@
 #![macro_use]
 #[allow(unused_macros)]
 macro_rules! input {
-    ($pr:expr, $type:ty) => {{
-	use std::io::Write;
-        print!($pr);
-	std::io::stdout().flush().expect("io error");
-        let mut line = String::new();
-        std::io::stdin().read_line(&mut line).expect("No input");
-        line.trim().parse::<$type>().expect("Bad input")
+    ($prompt:expr, $type:ty) => {{
+        input::<$type>($prompt)
     }};
 }
 

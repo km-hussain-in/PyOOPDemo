1a9577de7514a348ec2925a7c05de4786e26847b
K.M Hussain <km.hussain.in@gmail.com>
2020-08-17 09:39:12 +0530

    Uploaded from iPad

---
+++ Misc/console.rs
@@ -0,0 +1,25 @@
+#![macro_use]
+#[allow(unused_macros)]
+macro_rules! input {
+    ($pr:expr, $type:ty) => {{
+		use std::io::Write;
+        print!($pr);
+		std::io::stdout().flush().expect("io error");
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).expect("No input");
+        line.trim().parse::<$type>().expect("Bad input")
+    }};
+}
+
+
+pub fn input<T>(prompt: &'static str) -> T 
+where T: std::str::FromStr,
+T::Err: std::fmt::Debug,
+{
+	use std::io::Write;
+	print!("{}", prompt);
+	std::io::stdout().flush().expect("io error");
+	let mut line = String::new();
+	std::io::stdin().read_line(&mut line).expect("No input");
+	line.trim().parse::<T>().expect("Bad input")
+}
